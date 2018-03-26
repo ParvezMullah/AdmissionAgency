4389769da314aed5e10eea1ef52d7ea12f33d11a
unknown <rocksparvezmulla1@gmail.com>
2018-03-26 09:13:34 +0530

    rating done

---
+++ Admin/AgentRating.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AgentRating.aspx.cs" Inherits="AgentRating" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtemailid" runat="server"></asp:TextBox><asp:RequiredFieldValidator ID="RequiredFieldValidator1" ControlToValidate="txtemailid" runat="server" ErrorMessage="*" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:DropDownList ID="ddlPriority" runat="server">
+            <asp:ListItem Value="0">--Select Priority--</asp:ListItem>
+            <asp:ListItem>1</asp:ListItem>
+            <asp:ListItem>2</asp:ListItem>
+            <asp:ListItem>3</asp:ListItem>
+            <asp:ListItem>4</asp:ListItem>
+            <asp:ListItem>5</asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click" />
+        <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Admin/AgentRating.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class AgentRating : System.Web.UI.Page
+{
+    string connectionString = WebConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string showQuery = "select emailid,successcount,totalattempt from tblsuccessrate";
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand(showQuery, conn);
+            try
+            {
+                conn.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+            catch (Exception exc)
+            {
+                Response.Write(exc.Message);
+            }
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string updateQuery = "update tblagent set priority = @priority where emailid= @emailid";
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand(updateQuery, conn);
+            cmd.Parameters.AddWithValue("@priority", ddlPriority.SelectedValue.ToString());
+            cmd.Parameters.AddWithValue("@emailid",txtemailid.Text.ToString());
+
+            try
+            {
+                conn.Open();
+                int count = cmd.ExecuteNonQuery();
+                if(count > 0)
+                {
+                    lblMsg.Text = "Priority updated successfully!!";
+                }
+                else
+                {
+                    lblMsg.Text = "Invalid Emailid";
+                }
+            }
+            catch (Exception exc)
+            {
+                Response.Write(exc.Message);
+            }
+        }
+    }
+
+}
